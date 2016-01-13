78c71c6cef65975f3e659901b80f194c0a3bff04
TomAwezome <sonidd1@yahoo.com>
2016-01-13 03:38:50 -0500

    created an example (and hopefully soon in some way functional) drivetrain subsystem
    Signed-off-by: TomAwezome <sonidd1@yahoo.com>

---
+++ src/org/usfirst/frc/team4534/robot/subsystems/DriveTrain.java
@@ -0,0 +1,32 @@
+package org.usfirst.frc.team4534.robot.subsystems;
+import org.usfirst.frc.team4534.robot.Robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveTrain extends Subsystem {
+
+	private RobotDrive drive;
+	
+	// create instance of motor-configuration based robot drive
+	public DriveTrain() {
+		drive = new RobotDrive(0, 1);
+	}
+	
+	// enable tank drive operation of the drivetrain
+	public void tankDrive(Joystick joy) {
+		drive.tankDrive(joy.getY(),joy.getRawAxis(4));
+	}
+	
+	// sets motor speed to 0 on each motor
+	public void stop() {
+		drive.tankDrive(0, 0);
+	}
+	// Set the default command for a subsystem here.
+	public void initDefaultCommand() {
+		// setDefaultCommand(new MySpecialCommand());		
+	}
+}
