8f4b8804925f25a4b22e080048ce24b0422057d9
acwhi <acwhi@172.20.112.71>
2016-01-19 21:11:01 -0500

    started the drive mode class
    Signed-off-by: acwhi <acwhi@172.20.112.71>

---
+++ src/org/usfirst/frc/team4534/robot/util/DriveMode.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team4534.robot.util;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriveMode {
+	
+	//speedfinal = ((speedi / (prec + 1)) * (fast +1)) / 2
+	
+	
+
+}
