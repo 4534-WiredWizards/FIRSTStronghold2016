a4ec02ce3b360fa04d547a63779159dfd19d7b09
Benjamin <Benjamin@FALCON>
2016-01-21 18:52:25 -0500

    Accel code exists?
    Signed-off-by: Benjamin <Benjamin@FALCON>

--- src/org/usfirst/frc/team4534/robot/ControlSystem.java
+++ src/org/usfirst/frc/team4534/robot/ControlSystem.java
@@ -57,7 +57,7 @@ public class ControlSystem {
 				joystick.setRumble(RumbleType.kRightRumble, 0);
 			}
 			// update acceleration
-			currentJoyY = Maths.lerp(currentJoyY, getMoveAxisY(), 0.1);
+			currentJoyY = Maths.lerp(currentJoyY, getMoveAxisY(), 1-Math.pow(.5, delta));
 		}
 		oldTime = newTime;
 	}
