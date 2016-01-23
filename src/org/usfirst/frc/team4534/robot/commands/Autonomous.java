04fed5993102562d17c556496e4ca494a4f63032
Emma <Emma@Bob>
2016-01-23 10:46:22 -0500

    I made it turn

--- src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
@@ -1,13 +1,14 @@
 package org.usfirst.frc.team4534.robot.commands;
 
+import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class Autonomous extends CommandGroup {
     
     public  Autonomous() {
        addSequential(new AutoDriveStraight(1, .5));
-       
+       addSequential(new AutoDriveRotate(1, .5));
        addSequential(new AutoDriveStraight(1, .5)); 
-       
+       addSequential(new AutoDriveRotate(-1, .5));
     }
 }
