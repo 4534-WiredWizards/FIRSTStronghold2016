1f0e4f5b4048be47e91b0c10a66033b32172102d
Emma <Emma@Bob>
2016-01-23 13:40:45 -0500

    autonomous rotation command did not time out during testing, added isTimedOut boolean to isFinished
    Signed-off-by: Emma <Emma@Bob>

--- src/org/usfirst/frc/team4534/robot/commands/AutoDriveRotate.java
+++ src/org/usfirst/frc/team4534/robot/commands/AutoDriveRotate.java
@@ -30,7 +30,7 @@ public class AutoDriveRotate extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isTimedOut();
     }
 
     // Called once after isFinished returns true
