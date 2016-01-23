ae9860666ac8780e17bea9ff6aac556ec02944a7
Benjamin <Benjamin@FALCON>
2016-01-23 12:19:48 -0500

    Fixed an error resulting in a negative value of time.
    Signed-off-by: Benjamin <Benjamin@FALCON>

--- src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
@@ -9,6 +9,6 @@ public class Autonomous extends CommandGroup {
        addSequential(new AutoDriveStraight(1, .5));
        addSequential(new AutoDriveRotate(1, .5));
        addSequential(new AutoDriveStraight(1, .5)); 
-       addSequential(new AutoDriveRotate(-1, .5));
+       addSequential(new AutoDriveRotate(1, -.5));
     }
 }
