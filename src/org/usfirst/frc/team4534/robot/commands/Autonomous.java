e275e238422ca22106d840ad3ca01fd2b932d0c7
Emma <Emma@Bob>
2016-01-23 14:08:23 -0500

    the robot was moving too fast in autonomous so I lowered the speed

--- src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
+++ src/org/usfirst/frc/team4534/robot/commands/Autonomous.java
@@ -6,9 +6,9 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Autonomous extends CommandGroup {
     
     public  Autonomous() {
-       addSequential(new AutoDriveStraight(1, .5));
-       addSequential(new AutoDriveRotate(1, .5));
-       addSequential(new AutoDriveStraight(1, .5)); 
-       addSequential(new AutoDriveRotate(1, -.5));
+       addSequential(new AutoDriveStraight(1, .2));
+       addSequential(new AutoDriveRotate(1, .2));
+       addSequential(new AutoDriveStraight(1, .2)); 
+       addSequential(new AutoDriveRotate(1, -.2));
     }
 }
