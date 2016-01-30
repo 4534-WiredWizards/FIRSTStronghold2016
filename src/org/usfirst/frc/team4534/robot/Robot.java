440d86b24d84dcfd97fc8059177a3ca56bde4a45
Benjamin <Benjamin@FALCON>
2016-01-30 11:33:12 -0500

    Added AutoChoosers, using LinkedLists...
    Signed-off-by: Benjamin <Benjamin@FALCON>

--- src/org/usfirst/frc/team4534/robot/Robot.java
+++ src/org/usfirst/frc/team4534/robot/Robot.java
@@ -1,6 +1,10 @@
 package org.usfirst.frc.team4534.robot;
 
-import org.usfirst.frc.team4534.robot.commands.Autonomous;
+import java.util.LinkedList;
+
+import org.usfirst.frc.team4534.robot.commands.AutoDriveRotate;
+import org.usfirst.frc.team4534.robot.commands.AutoDriveStraight;
+import org.usfirst.frc.team4534.robot.commands.DriveStop;
 import org.usfirst.frc.team4534.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team4534.robot.subsystems.BallIntake;
 
@@ -8,9 +12,11 @@ import org.usfirst.frc.team4534.robot.util.MillisecondTimer;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -24,9 +30,19 @@ public class Robot extends IterativeRobot {
 	public static OI oi;
 
 	public static DriveTrain drivetrain;
-	Command autonomousCommand;
+	//Command auto;
+	CommandGroup autonomousCommands;
 	public static BallIntake ballintake;
-
+	
+	public LinkedList<SendableChooser> steps = new LinkedList<SendableChooser>();
+	private static int numSteps = 5; //How many steps we will have in the auto routine
+	/*
+	public SendableChooser step1; // use to select the first (of 5) auto routines to run.
+	public SendableChooser step2; // use to select the second (of 5) auto methods
+	public SendableChooser step3; // use to select the second (of 5) auto methods
+	public SendableChooser step4; // use to select the second (of 5) auto methods
+	public SendableChooser step5; // use to select the second (of 5) auto methods
+	*/
 	// public SendableChooser autoChooser;
 
 	/**
@@ -37,7 +53,38 @@ public class Robot extends IterativeRobot {
 		System.out.println("robotInit");
 
 		drivetrain = new DriveTrain();
+		ballintake = new BallIntake();
 		oi = new OI();
+		//step1 = new SendableChooser();
+		//step1.addDefault("Wait", new DriveStop());
+		//step1.addObject("Straight One Second", new AutoDriveStraight(1, .35));
+		//step1.addObject("Turn 1 Second", new AutoDriveRotate(1, .35));
+		//SmartDashboard.putData(step1.toString() + "choice:", step1);
+		
+		for(int i = 0; i < numSteps; i++){
+			steps.add(new SendableChooser());
+		}
+		
+		for(int q = 0; q < steps.size(); q++){        // This loop adds functions to each Auto Chooser.
+			steps.get(q).addDefault("Wait", new DriveStop());
+			steps.get(q).addObject("Straight One Second", new AutoDriveStraight(1, .35));
+			steps.get(q).addObject("Turn 1 Second", new AutoDriveRotate(1, .35));
+			SmartDashboard.putData(steps.get(q).toString() + "choice:", steps.get(q));
+		}
+		/*
+		for (SendableChooser autoSteps : steps) {
+			autoSteps.addDefault("Wait", new DriveStop());
+			autoSteps.addObject("Straight One Second", new AutoDriveStraight(1, .35));
+			autoSteps.addObject("Turn 1 Second", new AutoDriveRotate(1, .35));
+			SmartDashboard.putData(autoSteps.toString() + "choice:", autoSteps);
+		}
+		*/
+		//autoChooserSetup(step1);
+		//autoChooserSetup(step2);
+		//autoChooserSetup(step3);
+		//autoChooserSetup(step4);
+		//autoChooserSetup(step5);
+		
 		ControlSystem.init();
 		ControlSystem.rumbleTimeSet(1);
 
@@ -46,12 +93,12 @@ public class Robot extends IterativeRobot {
 		// SmartDashboard.putData((NamedSendable) oi);
 
 		// instantiate the command used for the autonomous period
-		autonomousCommand = new Autonomous();
+		autonomousCommands = new CommandGroup();
+		
 		// autoChooser = new SendableChooser();
 		// autoChooser.addObject("Drive Straight", new Autonomous());
 
 		// SmartDashboard.putData("Auto Mode", autoChooser);
-		SmartDashboard.putData(autonomousCommand);
 		SmartDashboard.putData(Scheduler.getInstance());
 	}
 
@@ -61,9 +108,26 @@ public class Robot extends IterativeRobot {
 
 	public void autonomousInit() {
 		// schedule the autonomous command (example)
-		if (autonomousCommand != null) {
+		if (autonomousCommands != null) {
+			autonomousCommands.cancel();
+		}
+		
+		for(int e = 0; e < steps.size(); e++){
+			autonomousCommands.addSequential((Command) steps.get(e).getSelected());
+			
+		}
+		
+		//auto = (Command) step1.getSelected();
+		//autonomousCommands.addSequential((Command) step1.getSelected());
+		//autonomousCommands.addSequential((Command) step2.getSelected());
+		//autonomousCommands.addSequential((Command) step3.getSelected());
+		//autonomousCommands.addSequential((Command) step4.getSelected());
+		//autonomousCommands.addSequential((Command) step5.getSelected());
+		
+		
+		if (autonomousCommands != null) {
 			// autonomousCommand = (Command) autoChooser.getSelected();
-			autonomousCommand.start();
+			autonomousCommands.start();
 		}
 	}
 
@@ -72,6 +136,7 @@ public class Robot extends IterativeRobot {
 	 */
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		
 	}
 
 	public void teleopInit() {
@@ -79,8 +144,8 @@ public class Robot extends IterativeRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		if (autonomousCommand != null) {
-			autonomousCommand.cancel();
+		if (autonomousCommands != null) {
+			autonomousCommands.cancel();
 		}
 	}
 
@@ -89,6 +154,10 @@ public class Robot extends IterativeRobot {
 	 * to reset subsystems before shutting down.
 	 */
 	public void disabledInit() {
+		if (autonomousCommands != null) {
+				autonomousCommands.cancel();
+			}
+		//auto.cancel();
 
 	}
 
@@ -111,4 +180,14 @@ public class Robot extends IterativeRobot {
 	public void testPeriodic() {
 		LiveWindow.run();
 	}
+
+private void autoChooserSetup(SendableChooser chooser) {
+	/*
+	chooser = new SendableChooser();
+	chooser.addDefault("Wait", new DriveStop());
+	chooser.addObject("Straight One Second", new AutoDriveStraight(1, .35));
+	chooser.addObject("Turn 1 Second", new AutoDriveRotate(1, .35));
+	SmartDashboard.putData(chooser.toString() + "choice:", chooser);
+	*/
+}
 }
