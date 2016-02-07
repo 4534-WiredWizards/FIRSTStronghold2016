9357ec8c47d2b84ba3d9cba62f0202b27a3f09b3
acwhi <acwhi@LAPTOP-A3DGAOJ6>
2016-02-07 13:48:17 -0500

    added arm Pneumatics
    Signed-off-by: acwhi <acwhi@LAPTOP-A3DGAOJ6>

--- src/org/usfirst/frc/team4534/robot/Robot.java
+++ src/org/usfirst/frc/team4534/robot/Robot.java
@@ -59,7 +59,7 @@ public class Robot extends IterativeRobot {
 		autoDefense.addObject("Rock Wall", new DriveStop());
 		autoDefense.addObject("Moat", new DriveStop());
 		autoDefense.addObject("Ramparts", new DriveStop());
-		autoDefense.addObject("Secret Passage", new AutoDriveStraight(1, .35));
+		autoDefense.addObject("Secret Passage", new AutoDriveStraight(3, .35));
 		SmartDashboard.putData("Auto Defense", autoDefense);
 		
 		//Right Now, only autoDefense will be used.
@@ -121,10 +121,7 @@ public class Robot extends IterativeRobot {
 			// autonomousCommand = (Command) autoChooser.getSelected();
 			autoDefenseChoice.start();
 			System.out.println("Auto Started!");
-		if(accelerometer.getZ() >= 2){
-			autoDefenseChoice.cancel();
-			System.out.println("Accelometer value greater than 2.");
-		}
+		
 		
 		}
 	}
@@ -134,6 +131,12 @@ public class Robot extends IterativeRobot {
 	 */
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		SmartDashboard.putNumber("Accelerometer", accelerometer.getZ());
+		
+		if(accelerometer.getZ() >= 3.0){
+			autoDefenseChoice.cancel();
+			System.out.println("Accelometer value greater than 2.");
+		}
 
 	}
 
