784449fd3063734f08f1bedc6677cee8806b4f46
Brandon Dyer <brandondyer64@gmail.com>
2016-02-07 14:08:21 -0500

    Added new button listeners.
    Signed-off-by: Brandon Dyer <brandondyer64@gmail.com>

---
+++ src/org/usfirst/frc/team4534/robot/controls/CommandButtonRelease.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4534.robot.controls;
+
+import org.usfirst.frc.team4534.robot.ControlSystem.Button;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class CommandButtonRelease implements ButtonListener {
+
+	private final Button button;
+	private final Command command;
+	
+	public CommandButtonRelease(Button button, Command command) {
+		this.button = button;
+		this.command = command;
+	}
+
+	@Override
+	public void onButtonPress(Button button, double value) {
+	}
+	
+	@Override
+	public void onButtonRelease(Button button) {
+		if (button.equals(this.button) && !command.isRunning()) {
+			command.start();
+		}
+	}
+
+}
